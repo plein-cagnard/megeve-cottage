13bb77d3bf92a1717752e1572c5c9a0e22dec249
arthur <arthureuw@gmail.com>
2022-06-17 14:20:35 +0200

    Fix: Refactor entity and upadted git ignore.

--- src/helpers/entity.js
+++ src/helpers/entity.js
@@ -1,22 +1,16 @@
 import API from './api';
 
-export const getEntities = (name) => {
-  API.get(`${name}`).then((response) => {
-    console.log(response);
-    return response;
-  });
+export const getEntities = async (name) => {
+  const res = await API.get(`${name}`);
+  return res.data;
 }
 
-export const getEntity = (name, id = '') => {
-  API.get(`${name}/${id}`).then((response) => {
-    console.log(response);
-    return response;
-  });
+export const getEntity = async (name, id = '') => {
+  const res = await API.get(`${name}/${id}`)
+  return res.data;
 }
 
-export const getSingle = (name) => {
-  API.get(`${name}`).then((response) => {
-    console.log(response);
-    return response;
-  });
+export const getSingle = async (name) => {
+  const res = await API.get(`${name}`);
+  return res.data;
 }
