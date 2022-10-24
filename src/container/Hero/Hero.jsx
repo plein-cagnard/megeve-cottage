8abdfed989f842613780401beaee8f2c21c2b791
arthur <arthureuw@gmail.com>
2022-10-24 18:27:26 +0200

    Added logo name and h2 mixins

--- src/container/Hero/Hero.jsx
+++ src/container/Hero/Hero.jsx
@@ -17,6 +17,9 @@ const Hero = ({scrollDown}) => {
   return (
     <header className="Hero">
         <img className='background-image' src={BASE_URL + '/uploads/Chalet_OA_3_908c02ac86.jpg'} alt="Mountain cottage" />
+        <div className="logo">
+            <h2>Megève Cottage</h2>
+        </div>
         <div className='content'>
             <h1 className='title'>Test Title Cottages</h1>
             <span className='subtitle'>Test SubTitle Cottages</span>
